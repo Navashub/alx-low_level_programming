22b0203777ec728bd5f3eff65e780a2f9bdc493b
Navashub <navasmuller01@gmail.com>
2022-12-09 07:14:11 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase followed by new line , except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ta;
+
+	for (ta = 'a'; ta <= 'z'; ta++)
+	{
+		if (ta != 'e' && ta != 'q')
+			putchar(ta);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
