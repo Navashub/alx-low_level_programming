c492df14cbfff2f95dc1486577b841be534f05e9
Navashub <navasmuller01@gmail.com>
2022-12-09 08:07:32 -0800

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,18 @@ int main(void)
 {
 	int n;
 
-	for (n = 28; n <= 38; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
-		if (n != 37)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar('\n');
-		return (0);
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
 	}
+
+	putchar('\n');
+
+	return (0);
 }
