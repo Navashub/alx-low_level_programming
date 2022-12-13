0d43856c6e65f7f62b0d7c1d23b9924d98dcff0f
Navashub <navasmuller01@gmail.com>
2022-12-13 03:59:09 -0800

    Alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void);
+{
+	char ta;
+
+	for (ta = 'a'; ta <= 'z'; ta++)
+		_putchar(ta);
+
+	_putchar('\n');
+}
