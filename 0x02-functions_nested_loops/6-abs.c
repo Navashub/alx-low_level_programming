2e8de3e7417ea39e071ce29e25cfddc6f670950c
Navashub <navasmuller01@gmail.com>
2022-12-13 20:25:07 -0800

    Last digit print

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
+ * _abs - Computes the absolute value of an integer
  * @t: The integer to be computed
  * Return: The absolute value
  */
