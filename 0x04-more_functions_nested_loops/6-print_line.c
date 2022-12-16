26b2e565d46dccce468f7405e325a883ec76a1b2
Navashub <navasmuller01@gmail.com>
2022-12-16 02:40:09 -0800

    printing line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * @n: number of times to print_,
+ * Return: no return
+ */
+
+void print_line(int n)
+{[
+
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
