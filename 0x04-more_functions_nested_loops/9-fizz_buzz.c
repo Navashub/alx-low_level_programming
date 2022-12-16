e30bf59f03c19f6ad7ea65353806fb7f523dc864
Navashub <navasmuller01@gmail.com>
2022-12-16 03:10:25 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,29 +8,33 @@
 
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 15 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (n % 3 == 0)
-		{
-			printf(" Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf(" Buzz");
-		}
 		else
 		{
-			printf(" %d", n);
+			printf("%d", i);
 		}
+		if (i == 100)
+		{
+			break;
+		}
+		else
+			printf(" ");
 	}
-	printf("\n")
-		return (0);
+	printf("\n");
+	return (0);
 }
