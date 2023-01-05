da4704e411cc29a2ac57591d110525913406fdfa
Navashub <navasmuller01@gmail.com>
2023-01-05 10:54:53 -0800

    Returns the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	int len = 0;
+
+	if (*s)
 	{
-		return (0);
-	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1);
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
 }
